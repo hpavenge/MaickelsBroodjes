02ecc01822d3c6fdf6aa46ea20140bea36953fbb
Nick van der Raaf <nickraaf@outlook.com>
2025-09-08 12:56:14 +0200

    Update Sauzen.tsx

--- app/components/Sauzen.tsx
+++ app/components/Sauzen.tsx
@@ -34,7 +34,7 @@ export default function Sauzen() {
               <img
                 src={saus.afbeelding}
                 alt={saus.alt}
-                className="w-full h-auto object-cover"
+                className="w-full h-[250px] object-contain"
               />
             </div>
             <div className="p-4">
