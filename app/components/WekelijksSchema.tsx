09bf0b646ea2ef25b5fb5645c523d480e44dfe59
Nick van der Raaf <nickraaf@outlook.com>
2025-11-10 08:47:50 +0100

    Update WekelijksSchema.tsx

--- app/components/WekelijksSchema.tsx
+++ app/components/WekelijksSchema.tsx
@@ -43,10 +43,10 @@ const locaties = {
     ],
     vrijdag: [
       {
-        naam: 'RVL Auto’s',
-        adres: 'Pater de Leeuwstraat 7, 5741 EE Beek en Donk',
+        naam: 'Hap B.V.',
+        adres: 'Scherpenbeemd 5, 5741 TX Beek en Donk',
         tijd: '12.00 – 13.30 uur',
-        maps: 'https://maps.google.com/?q=Bosscheweg+34,+Beek+en+Donk',
+        maps: 'https://www.google.com/maps/place/Scherpenbeemd+5,+5741+TX+Beek+en+Donk/',
       },
     ],
     zaterdag: [
